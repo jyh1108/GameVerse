09155af634987844dc29e968db3416a84b126587
jyh1108 <kobcom12@naver.com>
2024-04-09 20:52:45 +0900

    start

---
+++ src/main/java/com/example/gameverse/GameVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameVerseApplication.class, args);
+    }
+
+}
